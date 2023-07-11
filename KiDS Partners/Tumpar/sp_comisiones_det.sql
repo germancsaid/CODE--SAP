f13bfdbb2540c05d3abd1a414a4a08722e430cca
German C. Said Tardio <germantardiop@gmail.com>
2023-07-11 00:37:14 -0400

    V2 Reportes Tumpar

--- KiDS Partners/Tumpar/sp_comisiones_det.sql
+++ KiDS Partners/Tumpar/sp_comisiones_det.sql
@@ -1,5 +1,8 @@
 CREATE PROCEDURE KDS_COMISIONES_DET (in fdesde date, in fhasta date, in vendedor varchar(100))
 -- CALL KDS_COMISIONES_DET('2023-05-01','2023-05-31','CASTRO CUELLAR JOANA') 
+/*  modificaciones 
+    10-07-2023 v2
+*/
 AS
 BEGIN
 
@@ -15,7 +18,11 @@ FROM
     SELECT
       T1."DocDate" AS "Fecha", T1."DocNum" AS "Pago", 'Contado' AS "Tipo",
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END AS "Monto"
+      --CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END AS "Monto", modificaciones v2 10-07
+      CASE 
+      WHEN (T2."SumApplied" + T0."LocTotal" = 0) OR (T2."SumApplied" > ABS(T0."LocTotal")) THEN T0."LocTotal" 
+      WHEN (T2."SumApplied" + T0."LocTotal" <> 0) AND (T0."LocTotal" - T2."SumApplied" < 0) AND (T2."SumApplied" < ABS(T0."LocTotal")) THEN T2."SumApplied" * (-1)
+      ELSE  T2."SumApplied" END AS "Monto" --+linea modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
@@ -35,7 +42,11 @@ FROM
     SELECT
       T1."DocDate" AS "Fecha", T1."DocNum" AS "Pago", 'Credito' AS "Tipo",
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END AS "Monto"
+      --CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END AS "Monto", --modificaciones v2 10-07
+      CASE 
+      WHEN (T2."SumApplied" + T0."LocTotal" = 0) OR (T2."SumApplied" > ABS(T0."LocTotal")) THEN T0."LocTotal" 
+      WHEN (T2."SumApplied" + T0."LocTotal" <> 0) AND (T0."LocTotal" - T2."SumApplied" < 0) AND (T2."SumApplied" < ABS(T0."LocTotal")) THEN T2."SumApplied" * (-1)
+      ELSE  T2."SumApplied" END AS "Monto" --+linea modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
@@ -54,17 +65,18 @@ FROM
     --  PAGOS RECIBIDOS ANTICIPOS DETALLADO
     SELECT
       T1."DocDate" AS "Fecha", T1."DocNum" AS "Pago", 'Anticipo' AS "Tipo",
-      CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
+      T1."U_VENDEDOR" AS "Vendedor", --+linea modificaciones v2 10-07
+      --CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor", modificaciones v2 10-07
       CASE WHEN T1."NoDocSum" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T1."NoDocSum" END AS "Monto"
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
-      LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
-      LEFT JOIN OINV T3 ON T2."DocEntry" = T3."DocEntry"
-      LEFT JOIN OSLP T4 ON T3."SlpCode" = T4."SlpCode" 
+      --LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum" modificaciones v2 10-07
+      --LEFT JOIN OINV T3 ON T2."DocEntry" = T3."DocEntry" modificaciones v2 10-07
+      --LEFT JOIN OSLP T4 ON T3."SlpCode" = T4."SlpCode"  modificaciones v2 10-07
       WHERE T0."TransType"  = '24' AND
       T0."RefDate" >= fdesde AND 
       T0."RefDate" <= fhasta AND
-      (T3."DocDate" >= '2019-01-01' OR T3."DocDate" IS NULL OR T3."DocDate" = '') AND
+      --(T3."DocDate" >= '2019-01-01' OR T3."DocDate" IS NULL OR T3."DocDate" = '') AND modificaciones v2 10-07
       T1."PayNoDoc" = 'Y'
       
       UNION ALL
@@ -85,14 +97,15 @@ FROM
       T0."RefDate" >= fdesde AND 
       T0."RefDate" <= fhasta AND
       (T3."DocDate" >= '2019-01-01' OR T3."DocDate" IS NULL OR T3."DocDate" = '')
-      
+
       UNION ALL
       
     -- PAGOS EFECTUADOS NOTAS DE CREDITO DETALLADOS
     SELECT
       T1."DocDate" AS "Fecha", T1."DocNum" AS "Pago", 'Nota de credito' AS "Tipo",
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE -1 * T2."SumApplied" END AS "Monto"
+      CASE WHEN T0."LocTotal" > 0 THEN T2."SumApplied" * (-1) ELSE T2."SumApplied" END AS "Monto" --+linea modificaciones v2 10-07
+      --CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE -1 * T2."SumApplied" END AS "Monto" modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN OVPM T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN VPM2 T2 ON T1."DocEntry" = T2."DocNum"
--- KiDS Partners/Tumpar/sp_comisiones_tot.sql
+++ KiDS Partners/Tumpar/sp_comisiones_tot.sql
@@ -1,5 +1,8 @@
 CREATE PROCEDURE KDS_COMISIONES_TOT (in fdesde date, in fhasta date) 
 -- CALL KDS_COMISIONES_TOT('2023-05-01','2023-05-31') 
+/*  modificaciones 
+    10-07-2023 v2
+*/
 AS
 BEGIN
 
@@ -29,7 +32,11 @@ FROM
     -- PAGOS RECIBIDOS AL CONTADO AGRUPADO
     SELECT
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto"
+      --SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto" modificaciones v2 10-07
+      SUM(CASE 
+      WHEN (T2."SumApplied" + T0."LocTotal" = 0) OR (T2."SumApplied" > ABS(T0."LocTotal")) THEN T0."LocTotal" 
+      WHEN (T2."SumApplied" + T0."LocTotal" <> 0) AND (T0."LocTotal" - T2."SumApplied" < 0) AND (T2."SumApplied" < ABS(T0."LocTotal")) THEN T2."SumApplied" * (-1)
+      ELSE  T2."SumApplied" END) AS "Monto"--+linea modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
@@ -48,7 +55,11 @@ FROM
     -- PAGOS RECIBIDOS AL CREDITO AGRUPADO
     SELECT
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto"
+      --SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto", --modificaciones v2 10-07
+      SUM(CASE 
+      WHEN (T2."SumApplied" + T0."LocTotal" = 0) OR (T2."SumApplied" > ABS(T0."LocTotal")) THEN T0."LocTotal" 
+      WHEN (T2."SumApplied" + T0."LocTotal" <> 0) AND (T0."LocTotal" - T2."SumApplied" < 0) AND (T2."SumApplied" < ABS(T0."LocTotal")) THEN T2."SumApplied" * (-1)
+      ELSE  T2."SumApplied" END) AS "Monto"  --+linea modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
@@ -65,20 +76,24 @@ FROM
       ) CD ON U."SlpName" = CD."Vendedor"
       LEFT JOIN (
     --  PAGOS RECIBIDOS ANTICIPOS AGRUPADO
+
+
     SELECT 
-      CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
+      T1."U_VENDEDOR" AS "Vendedor", --+linea modificaciones v2 10-07
+      --CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor", modificaciones v2 10-07
       SUM(CASE WHEN T1."NoDocSum" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T1."NoDocSum" END) AS "Monto"
       FROM OJDT T0 
       LEFT JOIN ORCT T1 ON T0."BaseRef" = T1."DocNum"
-      LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum"
-      LEFT JOIN OINV T3 ON T2."DocEntry" = T3."DocEntry"
-      LEFT JOIN OSLP T4 ON T3."SlpCode" = T4."SlpCode" 
+      --LEFT JOIN RCT2 T2 ON T1."DocEntry" = T2."DocNum" modificaciones v2 10-07
+      --LEFT JOIN OINV T3 ON T2."DocEntry" = T3."DocEntry" modificaciones v2 10-07
+      --LEFT JOIN OSLP T4 ON T3."SlpCode" = T4."SlpCode"  modificaciones v2 10-07
       WHERE T0."TransType"  = '24' AND
       T0."RefDate" >= fdesde AND 
       T0."RefDate" <= fhasta AND
-      (T3."DocDate" >= '2019-01-01' OR T3."DocDate" IS NULL OR T3."DocDate" = '') AND
+      --(T3."DocDate" >= '2019-01-01' OR T3."DocDate" IS NULL OR T3."DocDate" = '') AND modificaciones v2 10-07
       T1."PayNoDoc" = 'Y'
-      GROUP BY CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END
+      --GROUP BY CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END
+	  GROUP BY T1."U_VENDEDOR"
       ) AN ON U."SlpName" = AN."Vendedor"
       LEFT JOIN (
     -- PAGOS RECIBIDOS OTROS AGRUPADOS
@@ -102,7 +117,8 @@ FROM
     -- PAGOS EFECTUADOS NOTAS DE CREDITO AGRUPADO
     SELECT
       CASE WHEN T4."SlpName" IS NOT NULL THEN T4."SlpName" ELSE T1."U_VENDEDOR" END AS "Vendedor",
-      SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto"
+      SUM(CASE WHEN T0."LocTotal" > 0 THEN T2."SumApplied" * (-1) ELSE T2."SumApplied" END) AS "Monto"--+linea modificaciones v2 10-07
+      --SUM(CASE WHEN T2."SumApplied" + T0."LocTotal" = 0 THEN T0."LocTotal" ELSE T2."SumApplied" END) AS "Monto" modificaciones v2 10-07
       FROM OJDT T0 
       LEFT JOIN OVPM T1 ON T0."BaseRef" = T1."DocNum"
       LEFT JOIN VPM2 T2 ON T1."DocEntry" = T2."DocNum"
